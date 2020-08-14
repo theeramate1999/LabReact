d7cb0b342389380b611f9ef7839dc043bc5fab20
theeramate1999 <6110110217@psu.ac.th>
2020-08-14 22:51:57 +0700

    past 5

--- card-game/src/CharacterCard.js
+++ card-game/src/CharacterCard.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function CharacterCard(props){
-     return(
-         <div className = "card">{props.value}</div>
-     )
+export default function CharacterCard(props) {
+   
+    const [active, setActive] = useState(false);
+    const activate = () => {
+    setActive(true)
+        if(!active){
+             setActive(true)
+             props.activationHandler(props.value)
+        }
+ }
+   
+    const className = `card ${active ? 'activeCard': ''}`
+ 
+ 
+    return (
+ <div className={className} onClick={activate}>{props.value}</div>
+ )
+
 }
